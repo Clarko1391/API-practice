f2c00dacd0d2602234f95422e4bfaa0e2a2eac1e
Clark Oake <clarkoake13@gmail.com>
2020-09-19 10:46:24 -0230

    Fixed data by country
    found a way to parse summary page to get current info by country. Functionality appears to be working

--- covidMonitor/js/app.js
+++ covidMonitor/js/app.js
@@ -26,27 +26,53 @@ fetch('https://api.covid19api.com/summary')
 //Create a variable from user Input
 document.getElementById('enterCountry').addEventListener('click', function() {getCountry()});
 let getCountry = () => {
-    let liveDateTo = new Date().toISOString().split('');
-    liveDateTo.splice(-5, 4);
-    liveDateTo = liveDateTo.join('')
-    let liveDateFrom = new Date().toISOString().split('');
-    liveDateFrom.splice(-13, 13);
-    liveDateFrom.push('00:00:00Z');
-    liveDateFrom = liveDateFrom.join('')
-    let countryInput = document.getElementById('countryInput').value.toLowerCase();
-    let url = `https://api.covid19api.com/live/country/${countryInput}/status/confirmed/date/2020-05-06T00:00:00Z`
-    fetch(url)
-        .then(response => {
-            if (response.ok) {
-                response = response.json()
-                .then(response => {
-                    // let countryStats = response.Countries.name;
-                    console.log(response);
-                    // console.log(countryStats);
-                });
-            } else { 
-                alert('please check country name and try again');
-            }
-        });
-}
+//     let liveDateTo = new Date().toISOString().split('');
+//     liveDateTo.splice(-5, 4);
+//     liveDateTo = liveDateTo.join('')
+//     let liveDateFrom = new Date().toISOString().split('');
+//     liveDateFrom.splice(-13, 13);
+//     liveDateFrom.push('00:00:00Z');
+//     liveDateFrom = liveDateFrom.join('')
+//     let countryInput = document.getElementById('countryInput').value.toLowerCase();
+//     let url = `https://api.covid19api.com/live/country/${countryInput}/status/confirmed/date/2020-05-06T00:00:00Z`
+//     fetch(url)
+//         .then(response => {
+//             if (response.ok) {
+//                 response = response.json()
+//                 .then(response => {
+//                     // let countryStats = response.Countries.name;
+//                     console.log(response);
+//                     // console.log(countryStats);
+//                 });
+//             } else { 
+//                 alert('please check country name and try again');
+//             }
+//         });
+// }
 
+fetch('https://api.covid19api.com/summary')
+    .then(response => {
+        if (response.ok) {
+            response = response.json()
+            .then(response => {
+                let countryInput = document.getElementById('countryInput').value.toLowerCase();
+                // let countrySummary;
+                let countryStats = response.Countries;
+                countryStats.forEach(country => {
+                    if (country.Slug == countryInput) {
+                        countryName.innerHTML = country.Country;
+                        newCases.innerHTML = country.NewConfirmed;
+                        activeCases.innerHTML = country.TotalConfirmed - country.TotalRecovered;
+                        recoveredCases.innerHTML = country.TotalRecovered;
+                        console.log(country);
+                    }
+                })
+                // newCases.innerHTML = globalStats.NewConfirmed;
+                // activeCases.innerHTML = globalStats.TotalConfirmed - globalStats.TotalRecovered;
+                // recoveredCases.innerHTML = globalStats.TotalRecovered;
+            });
+        } else {
+            console.log('Error, please try again');
+        }
+    });
+}
